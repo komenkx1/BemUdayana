d7fd1d8ddf0de886a2c87e9515fb8507388117e4
komenkx <komangpermana7@gmail.com>
2021-06-03 10:26:22 +0800

    fix redirect admin

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/';
-    public const ADMIN = '/admin';
+    public const ADMIN = '/bem-admin';
 
     /**
      * The controller namespace for the application.
